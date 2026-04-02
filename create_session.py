26cfc2b5354a2134f85edeef438acb121045f86b
oussgost6-eng <oussgost6@gmail.com>
2026-04-02 19:41:54 +0100

    Add files via upload

---
+++ create_session.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://www.facebook.com")
+    input("👉 Connecte-toi puis appuie sur ENTER...")
+
+    context.storage_state(path="session.json")
+    browser.close()
